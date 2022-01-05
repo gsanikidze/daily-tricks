33c82b9a3b692df9782385e7a2849b2f83602815
gsanikidze <sanikidze.guram@gmail.com>
2022-01-05 23:58:00 +0400

    [new] init next

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Daily Tricks</title>
+        <meta name="description" content="Daily Tricks" />
+      </Head>
+
+      <main>
+        <h1>
+          Some Code Samples
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
