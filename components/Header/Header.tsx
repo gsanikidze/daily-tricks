afef65ea5f23eb86aee8e17f597265af6d2eb73d
gsanikidze <sanikidze.guram@gmail.com>
2022-01-08 00:35:48 +0400

    [ref] auth hook

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,41 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { GitHub } from 'react-feather';
-import { GithubAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
-import { useDispatch } from 'react-redux';
 
 import Button from '../Button';
-import { logIn, getStoredUser, logOut } from '../../store/modules/user';
-import { useAppSelector } from '../../store';
+import useAuth from '../../hooks/useAuth';
 
 export default function Header() {
-  const dispatch = useDispatch();
-  const isAuthorized = useAppSelector((st) => st.user.isAuthorized);
-
-  const authWithGithub = async () => {
-    const provider = new GithubAuthProvider();
-    const auth = getAuth();
-
-    try {
-      const res = await signInWithPopup(auth, provider);
-      const credential = GithubAuthProvider.credentialFromResult(res);
-
-      if (credential) {
-        dispatch(logIn({
-          uid: res.user.uid,
-          accessToken: credential.accessToken,
-          displayName: res.user.displayName,
-          email: res.user.email,
-          photoURL: res.user.photoURL,
-        }));
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    dispatch(getStoredUser({}));
-  }, [dispatch]);
+  const { isAuthorized, authWithGithub, logOut } = useAuth();
 
   return (
     <header
@@ -47,7 +17,7 @@ export default function Header() {
         </h5>
         {
           isAuthorized ? (
-            <Button type="default" onClick={() => dispatch(logOut())}>
+            <Button type="default" onClick={logOut}>
               Log Out
             </Button>
           ) : (
---
+++ hooks/useAuth.ts
@@ -0,0 +1,48 @@
+import { getAuth, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../store';
+import { getStoredUser, logIn, logOut as logOutAction } from '../store/modules/user';
+
+const useAuth = () => {
+  const dispatch = useDispatch();
+  const isAuthorized = useAppSelector((st) => st.user.isAuthorized);
+
+  const authWithGithub = async () => {
+    const provider = new GithubAuthProvider();
+    const auth = getAuth();
+
+    try {
+      const res = await signInWithPopup(auth, provider);
+      const credential = GithubAuthProvider.credentialFromResult(res);
+
+      if (credential) {
+        dispatch(logIn({
+          uid: res.user.uid,
+          accessToken: credential.accessToken,
+          displayName: res.user.displayName,
+          email: res.user.email,
+          photoURL: res.user.photoURL,
+        }));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const logOut = () => {
+    dispatch(logOutAction());
+  };
+
+  useEffect(() => {
+    dispatch(getStoredUser({}));
+  }, [dispatch]);
+
+  return {
+    isAuthorized,
+    authWithGithub,
+    logOut,
+  };
+};
+
+export default useAuth;
