c49b1262b92c49d741bebc07f53c7ff96a142d40
gsanikidze <sanikidze.guram@gmail.com>
2022-01-23 00:08:37 +0400

    [new] get bookmarked tricks

---
+++ pages/api/bookmark/index.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+
+import { Trick, User } from '../../../db';
+import {
+  dbConnection, auth, router, Route, ThenType, getFbUser,
+} from '../../../apiUtils';
+
+const getBookmarks: Route<[
+  ThenType<typeof dbConnection>,
+  ThenType<typeof auth>,
+]> = {
+  matches: (req) => req.method === 'GET',
+  middleware: [dbConnection, auth],
+  handler: async (
+    req,
+    res,
+    middleware,
+  ) => {
+    const [connection, userId] = middleware;
+    const user = await connection.manager.findOne(User, { where: { fbId: userId as string } });
+
+    if (!user) {
+      return [404];
+    }
+
+    const records = await connection.manager.findByIds(Trick, user.bookmarkedTricks);
+    const populatedRecords = [];
+    const users: Record<string, any> = {};
+
+    for await (const record of records) {
+      if (!users[record.userId]) {
+        users[record.userId] = await getFbUser(record.userId);
+      }
+
+      populatedRecords.push({
+        ...record,
+        user: users[record.userId],
+      });
+    }
+
+    return [200, populatedRecords];
+  },
+};
+
+export default router([getBookmarks]);
